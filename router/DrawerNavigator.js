c5024f63dd40c94597957c9a65c399e4e5a0c99f
vitor <vitorvtlopes@gmail.com>
2024-10-14 08:47:19 -0300

    Inserindo navegação principal atráves do stack.

--- router/DrawerNavigator.js
+++ router/DrawerNavigator.js
@@ -1,5 +1,6 @@
 import { View, Text, TouchableOpacity} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer'; 
+import { createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from '../views/HomeScreen';
 import HeaderLeftMenu from '../components/HeaderLeftMenu';
 import Styling from '../assets/css/Styling';
@@ -9,6 +10,7 @@ import ViewProductScreen from '../views/ViewProductScreen';
 
 // Criar Drawer
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 // Conteúdo do Drawer
 const Filter = ({ navigation }) => {
@@ -19,7 +21,7 @@ const Filter = ({ navigation }) => {
             <TouchableOpacity style={Styling.leftBarButton}>
                 <Text style={Styling.leftBarButtonText}>Seco</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton} onPress={() => navigation.navigate('Home')}>
+            <TouchableOpacity style={Styling.leftBarButton}>
                 <Text style={Styling.leftBarButtonText}>Suave</Text>
             </TouchableOpacity>
             <TouchableOpacity style={Styling.leftBarButton}>
@@ -32,38 +34,43 @@ const Filter = ({ navigation }) => {
     );
   };
 
-// Navegação Drawer 
-function DrawerNavigator() {
+  function DrawerNavigatorr() {
     return (
       <Drawer.Navigator drawerContent={(props) => <Filter {...props} />}>
         <Drawer.Screen 
           name="Home" 
           component={HomeScreen} 
-          options={({ navigation }) => ({
+          options={{
             title: 'Início',
             headerStyle: { backgroundColor: '#661923' },
             headerTintColor: '#fff',
-          })}
+          }}
         />
-        <Drawer.Screen 
-          name="Cadastrar Produto" 
-          component={ProductRegistrationScreen}
-          options={({ navigation }) => ({
-            title: 'Cadastrar Produto',
-            headerStyle: { backgroundColor: '#661923' },
-            headerTintColor: '#fff',
-            headerLeft: () => <HeaderLeftMenu navigation={navigation} />,
-          })}/>
-        <Drawer.Screen
-          name="Visualizar Produto"
-          component={ViewProductScreen}
-          options={({ navigation }) => ({
-            title: 'Visualizar Produto',
-            headerStyle: { backgroundColor: '#661923' },
-            headerTintColor: '#fff',
-            headerLeft: () => <HeaderLeftMenu navigation={navigation} />,
-          })}/>
       </Drawer.Navigator>
+    );
+  }
+  
+  function DrawerNavigator() {
+    return (
+      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#661923' }, headerTintColor: '#fff'}}>
+        {/* Tela que contém o Drawer */}
+        <Stack.Screen 
+          name="HomeDrawer" 
+          component={DrawerNavigatorr} 
+          options={{ headerShown: false }}
+        />
+        {/* Outras telas usando Stack */}
+        <Stack.Screen 
+          name="Cadastrar Produto" 
+          component={ProductRegistrationScreen} 
+          options={{ title: 'Cadastrar Produto' }}
+        />
+        <Stack.Screen 
+          name="Visualizar Produto" 
+          component={ViewProductScreen} 
+          options={{ title: 'Visualizar Produto' }}
+        />
+      </Stack.Navigator>
     );
   }
 
