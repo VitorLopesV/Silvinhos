b286ec13076ed3a90424638f21f6d14b7e68c4b6
vitor <vitorvtlopes@gmail.com>
2024-10-16 08:33:09 -0300

    inserindo banco de dados

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 // IMPORTAÇÕES
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './router/DrawerNavigator';
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as SQLite from 'expo-sqlite';
 
 // Componente principal App
 function App() {
+
+  useEffect(() => {
+    async function setup(){
+      const db = await SQLite.openDatabaseAsync('Estoque');
+
+    await db.execAsync(`
+    PRAGMA journal_mode = WAL;
+    CREATE TABLE IF NOT EXISTS vinhos (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, tipo TEXT NOT NULL, preço DOUBLE NOT NULL, quantidade INTEGER NOT NULL, descrição TEXT);
+    INSERT INTO vinhos (name, tipo, preço, quantidade, descrição) VALUES ('Pérgola', 'Suave', 30,00, 3, 'Bom demais');
+    INSERT INTO vinhos (name, tipo, preço, quantidade, descrição) VALUES ('Cabernet', 'Tinto', 300,00, 1, 'Caro demais');
+    `);
+    const allRows = await db.getAllAsync('SELECT * FROM vinhos');
+    for (const row of allRows) {
+    console.log(row.id, row.name, row.tipo, row.preço, row.quantidade, row.descrição);
+    }}
+    setup();
+  }, []);
   return (
     <NavigationContainer>
       <DrawerNavigator />
