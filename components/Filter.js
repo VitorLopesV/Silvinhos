59a5708af52f43126fd540a6321199df39f1c196
vitor <vitorvtlopes@gmail.com>
2024-10-24 20:56:02 -0300

    Criando filtro ao pressionar os botões da left-bar

--- components/Filter.js
+++ components/Filter.js
@@ -1,6 +1,12 @@
 import { View, Text, TouchableOpacity} from 'react-native';
 import Styling from '../assets/css/Styling';
 import SearchBar from '../components/SearchBar';
+import db from '../util/db/db';
+
+
+const filterByType = (type) => {
+    db.getProductsFromType(type);
+}
 
 // Filtros da aba de filtro na tela principal.
 const Filter = () => {
@@ -8,25 +14,25 @@ const Filter = () => {
         <View style={Styling.drawerContainer}>
             <SearchBar/>
             <Text style={Styling.textTitle}>Tipos de vinhos:</Text>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('espumante')}>
                 <Text style={Styling.leftBarButtonText}>Espumante</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho branco')}>
                 <Text style={Styling.leftBarButtonText}>Branco</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho doce')}>
                 <Text style={Styling.leftBarButtonText}>Doce</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho rosé')}>
                 <Text style={Styling.leftBarButtonText}>Rosé</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho seco')}>
                 <Text style={Styling.leftBarButtonText}>Seco</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho tinto')}>
                 <Text style={Styling.leftBarButtonText}>Tinto</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={Styling.leftBarButton}>
+            <TouchableOpacity style={Styling.leftBarButton} onPress={() => filterByType('vinho verde')}>
                 <Text style={Styling.leftBarButtonText}>Verde</Text>
             </TouchableOpacity>
         </View>
